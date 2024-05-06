411b41512284fd0816f2f2be614b8f044354cc91
Leo <leonidms@gmail.com>
2024-05-06 04:52:20 +0300

    done

---
+++ internal/parser/rpn_test.go
@@ -0,0 +1,30 @@
+package parser_test
+
+import (
+	"testing"
+
+	"github.com/leonardinius/golox/internal/grammar"
+	"github.com/leonardinius/golox/internal/parser"
+	"github.com/leonardinius/golox/internal/scanner"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPrintRNPVisitor(t *testing.T) {
+	var tree parser.Expr = &parser.Binary{
+		Left: &parser.Unary{
+			Operator: scanner.NewToken(grammar.MINUS, "-", nil, 1),
+			Right: &parser.Literal{
+				Value: 123,
+			},
+		},
+		Operator: scanner.NewToken(grammar.STAR, "*", nil, 1),
+		Right: &parser.Grouping{
+			Expression: &parser.Literal{
+				Value: 45.67,
+			},
+		}}
+
+	p := parser.NewRPNPrinter()
+	out := p.Print(tree)
+	assert.Equal(t, "123 ~ 45.67 *", out)
+}
