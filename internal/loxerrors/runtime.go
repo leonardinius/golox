621f92cdd8deff964c3372745a97720608e85001
Leo <leonidms@gmail.com>
2024-05-08 20:46:50 +0300

    wip

--- internal/loxerrors/runtime.go
+++ internal/loxerrors/runtime.go
@@ -22,13 +22,13 @@ func NewRuntimeError(tok *token.Token, cause error) *RuntimeError {
 	return &RuntimeError{tok, cause}
 }
 
-func (r *RuntimeError) Unwrap() error {
-	return r.cause
-}
-
 // Error implements error.
 func (r *RuntimeError) Error() string {
 	return fmt.Sprintf("[line %d] at %s: %v", r.tok.Line, r.tok.Lexeme, r.cause)
+}
+
+func (r *RuntimeError) Unwrap() error {
+	return r.cause
 }
 
 var _ error = (*RuntimeError)(nil)
