837952b3245ba753b6241be3db1192a2c706b32a
Leo <leonidms@gmail.com>
2024-05-09 01:23:37 +0300

    wip

--- internal/interpreter/interpreter_test.go
+++ internal/interpreter/interpreter_test.go
@@ -16,39 +16,39 @@ func TestInterpret(t *testing.T) {
 		expectedOutput string
 		expectedError  string
 	}{
-		{name: `simple expression`, input: `1 + 2`, expectedOutput: `3`},
-		{name: `grouped`, input: `(1 + 2)`, expectedOutput: `3`},
-		{name: `nested`, input: `(1 + (2 + 3))`, expectedOutput: `6`},
-		{name: `precedence asterix`, input: `1 + 2 * 3`, expectedOutput: `7`},
-		{name: `precedence slash`, input: `1 + 9 / 3`, expectedOutput: `4`},
-		{name: `precedence asterix slash`, input: `1 + 2 * 6 / 4`, expectedOutput: `4`},
-		{name: `grouping nested precedence`, input: `((1 + 2) * 3)/2`, expectedOutput: `4.5`},
-		{name: `strings`, input: `"a" + "b"`, expectedOutput: `"ab"`},
-		{name: `boolean t`, input: `true`, expectedOutput: `true`},
-		{name: `boolean f`, input: `false`, expectedOutput: `false`},
-		{name: `bang`, input: `!false`, expectedOutput: `true`},
-		{name: `bang bang`, input: `!!false`, expectedOutput: `false`},
-		{name: `eqeq number`, input: `1 == 1`, expectedOutput: `true`},
-		{name: `eqeq number`, input: `1 == 2`, expectedOutput: `false`},
-		{name: `eqeq string`, input: `"a" == "a"`, expectedOutput: `true`},
-		{name: `eqeq string`, input: `"a" == "b"`, expectedOutput: `false`},
-		{name: `bangeq number`, input: `1 != 1`, expectedOutput: `false`},
-		{name: `bangeq number`, input: `1 != 2`, expectedOutput: `true`},
-		{name: `bangeq string`, input: `"a" != "a"`, expectedOutput: `false`},
-		{name: `bangeq string`, input: `"a" != "b"`, expectedOutput: `true`},
-		{name: `lt number`, input: `1 < 2`, expectedOutput: `true`},
-		{name: `lt number`, input: `1 < 1`, expectedOutput: `false`},
-		{name: `lte number`, input: `2 <= 1`, expectedOutput: `false`},
-		{name: `lte number`, input: `1 <= 1`, expectedOutput: `true`},
-		{name: `gt number`, input: `2 > 1`, expectedOutput: `true`},
-		{name: `gt number`, input: `1 > 1`, expectedOutput: `false`},
-		{name: `gte number`, input: `1 >= 2`, expectedOutput: `false`},
-		{name: `gte number`, input: `1 >= 1`, expectedOutput: `true`},
-		{name: `invalid expression`, input: `1 + 2 +`, expectedError: `parse error at ';': expected expression`},
-		{name: `invalid expression sum`, input: `"a" + 0`, expectedError: `at +: Operands must be two numbers or two strings.`},
-		{name: `invalid expression minus`, input: `0 - ""`, expectedError: `at -: Operands must be numbers.`},
-		{name: `invalid expression minus string`, input: `-"a"`, expectedError: `at -: Operand must be a number.`},
-		{name: `bang as boolean`, input: `!"a"`, expectedOutput: `false`},
+		{name: `simple expression`, input: `1 + 2;`, expectedOutput: `3`},
+		{name: `grouped`, input: `(1 + 2);`, expectedOutput: `3`},
+		{name: `nested`, input: `(1 + (2 + 3));`, expectedOutput: `6`},
+		{name: `precedence asterix`, input: `1 + 2 * 3;`, expectedOutput: `7`},
+		{name: `precedence slash`, input: `1 + 9 / 3;`, expectedOutput: `4`},
+		{name: `precedence asterix slash`, input: `1 + 2 * 6 / 4;`, expectedOutput: `4`},
+		{name: `grouping nested precedence`, input: `((1 + 2) * 3)/2;`, expectedOutput: `4.5`},
+		{name: `strings`, input: `"a" + "b";`, expectedOutput: `"ab"`},
+		{name: `boolean t`, input: `true;`, expectedOutput: `true`},
+		{name: `boolean f`, input: `false;`, expectedOutput: `false`},
+		{name: `bang`, input: `!false;`, expectedOutput: `true`},
+		{name: `bang bang`, input: `!!false;`, expectedOutput: `false`},
+		{name: `eqeq number`, input: `1 == 1;`, expectedOutput: `true`},
+		{name: `eqeq number`, input: `1 == 2;`, expectedOutput: `false`},
+		{name: `eqeq string`, input: `"a" == "a";`, expectedOutput: `true`},
+		{name: `eqeq string`, input: `"a" == "b";`, expectedOutput: `false`},
+		{name: `bangeq number`, input: `1 != 1;`, expectedOutput: `false`},
+		{name: `bangeq number`, input: `1 != 2;`, expectedOutput: `true`},
+		{name: `bangeq string`, input: `"a" != "a";`, expectedOutput: `false`},
+		{name: `bangeq string`, input: `"a" != "b";`, expectedOutput: `true`},
+		{name: `lt number`, input: `1 < 2;`, expectedOutput: `true`},
+		{name: `lt number`, input: `1 < 1;`, expectedOutput: `false`},
+		{name: `lte number`, input: `2 <= 1;`, expectedOutput: `false`},
+		{name: `lte number`, input: `1 <= 1;`, expectedOutput: `true`},
+		{name: `gt number`, input: `2 > 1;`, expectedOutput: `true`},
+		{name: `gt number`, input: `1 > 1;`, expectedOutput: `false`},
+		{name: `gte number`, input: `1 >= 2;`, expectedOutput: `false`},
+		{name: `gte number`, input: `1 >= 1;`, expectedOutput: `true`},
+		{name: `invalid expression`, input: `1 + 2 +;`, expectedError: `parse error at ';': expected expression`},
+		{name: `invalid expression sum`, input: `"a" + 0;`, expectedError: `at +: Operands must be two numbers or two strings.`},
+		{name: `invalid expression minus`, input: `0 - "";`, expectedError: `at -: Operands must be numbers.`},
+		{name: `invalid expression minus string`, input: `-"a";`, expectedError: `at -: Operand must be a number.`},
+		{name: `bang as boolean`, input: `!"a";`, expectedOutput: `false`},
 	}
 
 	for _, tc := range testcases {
@@ -66,7 +66,7 @@ func TestInterpret(t *testing.T) {
 
 func evaluate(script string) (string, error) {
 	eval := interpreter.NewInterpreter()
-	scan := scanner.NewScanner(script + ";")
+	scan := scanner.NewScanner(script)
 
 	tokens, err := scan.Scan()
 	if err != nil {
